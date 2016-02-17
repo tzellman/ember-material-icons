a2f8dbbef194b8e5218a58a483799befe754ada6
Jonathan Steele <jonathan@steele.pro>
2016-02-17 11:56:47 +0000

    Fixing helper to use new library

--- addon/helpers/material-icon-helper.js
+++ addon/helpers/material-icon-helper.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function materialIconHelper(params) {
   if (Ember.isEmpty(params)) return;
 
-  return Ember.String.htmlSafe(`<i class="zmdi zmdi-${params[0]}">`);
+  return Ember.String.htmlSafe(`<i class="material-icons">${params[0]}</i>`);
 }
 
 export default Ember.Helper.helper(materialIconHelper);
