2d6042b0fcc82c96becce04f03e22216a0ba60f4
Jonathan Steele <jonathan@steele.pro>
2016-02-17 10:53:11 +0000

    Removing Bower dependency and using Google icon font

--- index.js
+++ index.js
@@ -7,21 +7,6 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/material-design-iconic-font/dist/fonts/Material-Design-Iconic-Font.eot', {
-        destDir: 'fonts'
-    });
-    app.import(app.bowerDirectory + '/material-design-iconic-font/dist/fonts/Material-Design-Iconic-Font.svg', {
-      destDir: 'fonts'
-    });
-    app.import(app.bowerDirectory + '/material-design-iconic-font/dist/fonts/Material-Design-Iconic-Font.ttf', {
-      destDir: 'fonts'
-    });
-    app.import(app.bowerDirectory + '/material-design-iconic-font/dist/fonts/Material-Design-Iconic-Font.woff', {
-      destDir: 'fonts'
-    });
-    app.import(app.bowerDirectory + '/material-design-iconic-font/dist/fonts/Material-Design-Iconic-Font.woff2', {
-      destDir: 'fonts'
-    });
-    app.import(app.bowerDirectory + '/material-design-iconic-font/dist/css/material-design-iconic-font.min.css');
+    app.import('vendor/material-icons.css');
   }
 };
