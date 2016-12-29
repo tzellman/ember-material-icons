867464ec1d046591cd7b9fb7f383427666441cda
Tom Zellman <tzellman@gmail.com>
2016-12-28 20:13:57 -0500

    expose some attribute bindings + default aria-hidden attribute to true

--- addon/components/material-icon.js
+++ addon/components/material-icon.js
@@ -1,17 +1,24 @@
 import Ember from 'ember';
 import layout from '../templates/components/material-icon';
+const {Component, computed} = Ember;
 
-var MaterialIconComponent = Ember.Component.extend({
+var MaterialIconComponent = Component.extend({
   layout: layout,
 
   tagName: 'i',
   classNames: ['material-icons'],
+  attributeBindings: ['title', 'style', 'alt', 'ariaHiddenAttribute:aria-hidden'],
 
-  iconClass: Ember.computed('icon', 'positionalIcon', function() {
+  iconClass: Ember.computed('icon', 'positionalIcon', function () {
     var icon = this.getWithDefault('positionalIcon', this.get('icon'));
-    
+
     return icon;
+  }),
+
+  ariaHiddenAttribute: computed('ariaHidden', function () {
+    return this.get('ariaHidden') !== false ? 'true' : undefined;
   })
+
 });
 
 MaterialIconComponent.reopenClass({
